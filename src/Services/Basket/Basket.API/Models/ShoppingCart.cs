74d8817b3d601c06b5b8db0d39ca3c85522c5deb
Sleem Mostafa <Sleem.Mostafa@cybral.com>
2024-06-29 22:45:32 +0300

    Feat: handle basket service to mange cart item and add redis cache

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public required string UserName { get; init; }
+        public List<ShoppingCartItem> Items { get; init; } = [];
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        //Required for Mapping
+        public ShoppingCart()
+        {
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCartItem
+    {
+        public required int Quantity { get; init; } 
+        public required string Color { get; init; } 
+        public required decimal Price { get; init; } 
+        public required Guid ProductId { get; init; } 
+        public required string ProductName { get; init; } 
+    }
+}
